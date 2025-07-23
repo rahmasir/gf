dbd0c90832f237079089031a6992ec63324991d1
EnAnsari <rahmat.ansari.dev@gmail.com>
2025-07-23 12:36:09 +0330

    fix: some changes

--- git_flow/commands/push.py
+++ git_flow/commands/push.py
@@ -42,7 +42,7 @@ def push(
             feedback = f"Pushing branch ('{branch}') to current remote '{current_remote}'..."
         else:
             command.extend([current_remote, current_branch])
-            feedback = f"Pushing current branch ('{branch}') to current remote '{current_remote}'..."
+            feedback = f"Pushing current branch ('{current_branch}') to current remote '{current_remote}'..."
 
         typer.secho(feedback, fg=typer.colors.YELLOW)
         
